41be8e5fe1644d82ff1ba46eb6fb08a1c935c360
Daniel Li <7157019+dan-li-dev@users.noreply.github.com>
2025-12-31 22:49:41 +0100

    Add various statements to static.py
    Added multiple lines of text to static.py.

--- static/static.py
+++ static/static.py
@@ -1,3 +1,22 @@
 HTML = """
-Whatever you want
+The beekeeper was so confident that she didn't bother to wear protective gear.
+We'll allow a 5 percent discount off list prices.
+I’d like to honor an event.
+She wondered how many people actually bought the perch they kept behind the counter at coffee shops.
+I like crawfish but oysters suck.
+He is the happiest man alive.
+When John goes on a date, he's known to be a big spender.
+He can hardly speak.
+The windows looked out over the backyard.
+He got a fantastic performance review.
+The words you just used were mean.
+The gum was stuck under the desk and I couldn't get it off.
+You should eat them.
+Whitening your teeth can be bad for them.
+Tom wasn't that big a fool.
+Those pants are too tight.
+What a big eater he is!
+What a big truck!
+How dare you!
+It's quite a big deal, isn't it?
 """
